3ef4a92dbbf796ce1172d1d0b4401ee06dddbc25
yuriko-aya <thechildoflily@gmail.com>
2023-03-26 04:26:45 +0700

    Remove google recaptcha

--- config.sample.php
+++ config.sample.php
@@ -18,29 +18,3 @@ $images = '{*.png,*.jpg,*.jpeg,*.gif}';
 $allowedMime = array('image/png', 'image/jpeg', 'image/pjpeg', 'image/gif');
 $baseurl = $_SERVER['HTTP_HOST'].dirname($_SERVER['REQUEST_URI']).'/'.$filedir;
 
-//reChaptcha Configuration
-
-require_once "recaptchalib.php";
-
-// your secret key
-$secret = "";
-
-$site_key = "";
-
-// empty response
-$response = null;
- 
-// check secret key
-$reCaptcha = new ReCaptcha($secret);
-
-// if submitted check response
-if($_SERVER['REQUEST_METHOD'] == 'POST') {
-  if ($_POST["g-recaptcha-response"]) {
-    $response = $reCaptcha->verifyResponse(
-        $_SERVER["REMOTE_ADDR"],
-        $_POST["g-recaptcha-response"]
-    );
-  }
-}
-
-?>
